3a2060201f2f47214fe150313a6b2bec45eac6af
matteopinfildi <pinfildimatteo@gmail.com>
2025-03-08 10:49:54 +0100

    Fine grafica HomePage

--- src/main/java/org/example/ispwproject/control/HomePageController.java
+++ src/main/java/org/example/ispwproject/control/HomePageController.java
@@ -1,19 +1,47 @@
 package org.example.ispwproject.control;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
 
 
 public class HomePageController {
     @FXML
     private ImageView bannerImage;
+
+    @FXML
+    private ImageView postPWImage;
+
+    @FXML
+    private ImageView buyCPImage;
+
+    @FXML
+    private ImageView buyPLImage;
+
     public void initialize() {
-        try {
             Image banner = new Image(getClass().getResource("/org/example/ispwproject/image/banner.JPG").toExternalForm());
             bannerImage.setImage(banner);
-        } catch (NullPointerException e) {
-            System.err.println("Impossibile trovare il file banner.jpg. Controlla il percorso.");
-        }
+
+            Image pokeWall = new Image(getClass().getResource("/org/example/ispwproject/image/pokeWall.JPG").toExternalForm());
+            postPWImage.setImage(pokeWall);
+
+            Image classicPoke = new Image(getClass().getResource("/org/example/ispwproject/image/buyClassicPoke.JPG").toExternalForm());
+            buyCPImage.setImage(classicPoke);
+
+            Image pokeLab = new Image(getClass().getResource("/org/example/ispwproject/image/buyPokeLab.JPG").toExternalForm());
+            buyPLImage.setImage(pokeLab);
     }
+
+    @FXML
+    public void handleBuyPokeLab(ActionEvent event) {
+        System.out.println("Crea il tuo pokè!");
+    }
+
+    @FXML
+    public void handlePostOnPokeWall(ActionEvent event) {
+        System.out.println("Posta su pokè wall");
+    }
+
 }
