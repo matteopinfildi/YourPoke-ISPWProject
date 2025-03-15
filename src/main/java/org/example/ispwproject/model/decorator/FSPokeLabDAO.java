20767a8bd5739c22495b121d65ead4481672d1af
matteopinfildi <pinfildimatteo@gmail.com>
2025-03-15 10:31:52 +0100

    Aggiunta DBUserDAO

---
+++ src/main/java/org/example/ispwproject/model/decorator/FSPokeLabDAO.java
@@ -0,0 +1,20 @@
+package org.example.ispwproject.model.decorator;
+
+import org.example.ispwproject.utils.exception.SystemException;
+
+public class FSPokeLabDAO implements PokeLabDAO{
+    @Override
+    public void create(PokeLab pokeLab) throws SystemException {
+        // da implementare
+    }
+
+    @Override
+    public PokeLab read(int plid) throws SystemException {
+        return null;
+    }
+
+    @Override
+    public void delete(int plid) throws SystemException {
+        // da implementare
+    }
+}
---
+++ src/main/java/org/example/ispwproject/model/user/DBUserDAO.java
@@ -0,0 +1,103 @@
+package org.example.ispwproject.model.user;
+
+import org.example.ispwproject.model.decorator.DBPokeLabDAO;
+import org.example.ispwproject.model.decorator.PokeLab;
+import org.example.ispwproject.utils.enumeration.UserType;
+import org.example.ispwproject.utils.enumeration.ingredient.GenericAlternative;
+import org.example.ispwproject.utils.exception.SystemException;
+import org.example.ispwproject.utils.database.dbConnection;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class DBUserDAO implements UserDAO{
+
+    @Override
+    public void create (User user) throws SystemException{
+
+        String checkUserQuery = "SELECT COUNT(*) FROM users WHERE username = ?";
+        String insertUserQuery = "INSERT INTO users (username, password, email, utype, address, plid) VALUES (?, ?, ?, ?, ?, NULL)";
+        try (Connection connection = dbConnection.getDBConnection(); PreparedStatement preparedStatementCheck = connection.prepareStatement(checkUserQuery); PreparedStatement preparedStatementInsert = connection.prepareStatement(insertUserQuery)){
+            preparedStatementCheck.setString(1, user.getUsername());
+            ResultSet resultSet = preparedStatementCheck.executeQuery();
+            resultSet.next();
+            int counter = resultSet.getInt(1);
+
+            if (counter == 0){
+                preparedStatementInsert.setString(1, user.getUsername());
+                preparedStatementInsert.setString(2, user.getPassword());
+                preparedStatementInsert.setString(3, user.getEmail());
+                preparedStatementInsert.setString(4, user.getuType().name());
+                preparedStatementInsert.setString(5, user.getAddress());
+                preparedStatementInsert.executeUpdate();
+            }
+        } catch (SQLException e) {
+            throw new SystemException("Error create!");
+        }
+    }
+
+    @Override
+    public User read(String Uid) throws SystemException{
+        String queryRead = "SELECT * FROM users WHERE username = ?";
+        try(Connection connection = dbConnection.getDBConnection(); PreparedStatement preparedStatement =connection.prepareStatement(queryRead)){
+            preparedStatement.setString(1, Uid);
+            ResultSet resultSet = preparedStatement.executeQuery();
+
+            if (resultSet.next()){
+                int plid = resultSet.getInt("plid");
+                System.out.println(plid);
+                PokeLab pokeLab = plid != -1 ? new DBPokeLabDAO().read(plid) : null;
+
+                String username = resultSet.getString("username");
+                String password = resultSet.getString("password");
+                String email = resultSet.getString("email");
+                UserType userType = UserType.valueOf(resultSet.getString("utype"));
+                String address = resultSet.getString("address");
+
+                User user = new User(username, password, email, userType, address);
+                user.setPokeLab(pokeLab);
+                return user;
+            }else{
+                return null;
+            }
+        }catch (SQLException e) {
+            throw new SystemException("Error read!");
+        }
+    }
+
+    @Override
+    public void update(User user, int plid) throws SystemException {
+        String queryUpdate = "UPDATE users SET plid = ? WHERE username = ?";
+        try (Connection connection = dbConnection.getDBConnection(); PreparedStatement preparedStatement = connection.prepareStatement(queryUpdate)) {
+            deletePokeLab(user);
+            preparedStatement.setInt(1, plid);
+            preparedStatement.setString(2, user.getUsername());
+            preparedStatement.executeUpdate();
+        } catch (SQLException e) {
+            throw new SystemException("Errore update!");
+        }
+    }
+
+    private void deletePokeLab(User user) throws SystemException {
+        String query = "UPDATE users SET plid = NULL WHERE username = ?";
+        try (Connection connection = dbConnection.getDBConnection(); PreparedStatement preparedStatement = connection.prepareStatement(query)) {
+            preparedStatement.setString(1, user.getUsername());
+            preparedStatement.executeUpdate();
+        } catch (SQLException e) {
+            throw new SystemException("Errore delete poke lab!");
+        }
+    }
+
+    @Override
+    public void delete (String Uid) throws SystemException {
+        String queryDelete = "DELETE FROM users WHERE username = ?";
+        try (Connection connection = dbConnection.getDBConnection(); PreparedStatement preparedStatement = connection.prepareStatement(queryDelete)){
+            preparedStatement.setString(1, Uid);
+            preparedStatement.executeUpdate();
+        }catch (SQLException e) {
+            throw new SystemException("Errore delete user!");
+        }
+    }
+}
