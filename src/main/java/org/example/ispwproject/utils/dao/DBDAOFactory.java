d43d6a3a17f5c2d9139418027f3c7ab1dca23792
matteopinfildi <pinfildimatteo@gmail.com>
2025-04-30 19:00:50 +0200

    Modifica DB DAO Factory e aggiunta di un test per controllo connessione DB

--- src/main/java/org/example/ispwproject/utils/dao/DBDAOFactory.java
+++ src/main/java/org/example/ispwproject/utils/dao/DBDAOFactory.java
@@ -1,19 +1,19 @@
 package org.example.ispwproject.utils.dao;
 
-import org.example.ispwproject.model.decorator.pokelab.InMemoryPokeLabDAO;
+import org.example.ispwproject.model.decorator.pokelab.DBPokeLabDAO;
 import org.example.ispwproject.model.decorator.pokelab.PokeLabDAO;
 import org.example.ispwproject.model.decorator.spicy.InMemorySpicyDAO;
 import org.example.ispwproject.model.decorator.spicy.SpicyDAO;
 import org.example.ispwproject.model.decorator.topping.InMemoryToppingDAO;
 import org.example.ispwproject.model.decorator.topping.ToppingDAO;
-import org.example.ispwproject.model.observer.pokewall.InMemoryPokeWallDAO;
+import org.example.ispwproject.model.observer.pokewall.DBPokeWallDAO;
 import org.example.ispwproject.model.observer.pokewall.PokeWallDAO;
-import org.example.ispwproject.model.user.InMemoryUserDAO;
+import org.example.ispwproject.model.user.DBUserDAO;
 import org.example.ispwproject.model.user.UserDAO;
 
 public class DBDAOFactory extends DAOFactory{
     @Override
-    public PokeLabDAO getPokeLabDAO(){return InMemoryPokeLabDAO.getInstance();}
+    public PokeLabDAO getPokeLabDAO(){return new DBPokeLabDAO();}
 
     @Override
     public ToppingDAO getToppingDAO(){return InMemoryToppingDAO.getInstance();}
@@ -22,8 +22,8 @@ public class DBDAOFactory extends DAOFactory{
     public SpicyDAO getSpicyDAO(){return InMemorySpicyDAO.getInstance();}
 
     @Override
-    public UserDAO getUserDAO(){return InMemoryUserDAO.getInstance();}
+    public UserDAO getUserDAO(){return new DBUserDAO();}
 
     @Override
-    public PokeWallDAO getPokeWallDAO() {return InMemoryPokeWallDAO.getInstance();}
+    public PokeWallDAO getPokeWallDAO() {return new DBPokeWallDAO();}
 }
---
+++ src/test/java/org/example/ispwprogect/TestConnectionDB.java
@@ -0,0 +1,22 @@
+package org.example.ispwprogect;
+
+import org.example.ispwproject.utils.database.DBConnection;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class TestConnectionDB {
+
+    @Test
+    void testGetConnection() throws org.example.ispwproject.utils.exception.SystemException {
+
+        int value = 0;
+
+        if (DBConnection.getDBConnection() != null) {
+            value = 1;
+        }
+
+        Assertions.assertEquals(1, value);
+
+    }
+
+}
