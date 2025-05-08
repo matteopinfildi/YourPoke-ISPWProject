621e9f1120100c7047f8ab09ace7581ff4bc7ce8
matteopinfildi <pinfildimatteo@gmail.com>
2025-05-08 16:13:16 +0200

    Modifica

--- src/main/java/org/example/ispwproject/model/PokeWallSubject.java
+++ src/main/java/org/example/ispwproject/model/PokeWallSubject.java
@@ -1,6 +1,5 @@
 package org.example.ispwproject.model;
 
-import org.example.ispwproject.model.observer.pokewall.PokeWall;
 
 public interface PokeWallSubject {
     void registerObserver(PokeWallObserver observer);
