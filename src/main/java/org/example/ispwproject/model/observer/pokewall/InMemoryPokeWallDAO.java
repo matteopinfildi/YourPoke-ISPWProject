41b821ff5d517014cf529563bc2842b869daf187
matteopinfildi <pinfildimatteo@gmail.com>
2025-05-08 14:33:20 +0200

    modifica

--- src/main/java/org/example/ispwproject/model/observer/pokewall/InMemoryPokeWallDAO.java
+++ src/main/java/org/example/ispwproject/model/observer/pokewall/InMemoryPokeWallDAO.java
@@ -1,38 +1,3 @@
-//package org.example.ispwproject.model.observer.pokewall;
-//
-//import java.util.ArrayList;
-//import java.util.List;
-//
-//public class InMemoryPokeWallDAO implements PokeWallDAO {
-//    private static InMemoryPokeWallDAO instance;
-//    private List<PokeWall> posts = new ArrayList<>();
-//
-//    private InMemoryPokeWallDAO() {}
-//
-//    public static synchronized InMemoryPokeWallDAO getInstance() {
-//        if (instance == null) {
-//            instance = new InMemoryPokeWallDAO();
-//        }
-//        return instance;
-//    }
-//
-//    @Override
-//    public void create(PokeWall pokeWall) {
-//        pokeWall = new PokeWall(pokeWall.getPokeName(), pokeWall.getSize(), pokeWall.getUsername(), pokeWall.getIngredients());
-//        posts.add(pokeWall);
-//    }
-//
-//    @Override
-//    public List<PokeWall> getAllPosts() {
-//        return new ArrayList<>(posts);
-//    }
-//
-//    @Override
-//    public void delete(int postId) {
-//        posts.removeIf(post -> post.getId() == postId);
-//    }
-//}
-
 package org.example.ispwproject.model.observer.pokewall;
 
 import org.example.ispwproject.utils.exception.SystemException;
