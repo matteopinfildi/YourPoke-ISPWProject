872469033da5295bade8584436160b5355d2814c
matteopinfildi <pinfildimatteo@gmail.com>
2025-05-10 12:16:52 +0200

    Pulizia issues

--- src/main/java/org/example/ispwproject/model/user/DBUserDAO.java
+++ src/main/java/org/example/ispwproject/model/user/DBUserDAO.java
@@ -37,9 +37,11 @@ public class DBUserDAO implements UserDAO{
         }
     }
 
+
     @Override
     public User read(String uid) throws SystemException {
         String queryRead = "SELECT username, password, email, utype, address, plid FROM users WHERE username = ?";
+
         try (Connection connection = DBConnection.getDBConnection();
              PreparedStatement preparedStatement = connection.prepareStatement(queryRead)) {
 
@@ -50,17 +52,8 @@ public class DBUserDAO implements UserDAO{
                 // Leggi plid
                 int plid = resultSet.getInt("plid");
 
-                // Verifica che plid non sia nullo (o zero)
-                PokeLab pokeLab = null;
-                if (!resultSet.wasNull() && plid > 0) {
-                    // Se plid è valido, prova a caricare il PokeLab
-                    try {
-                        pokeLab = new DBPokeLabDAO().read(plid);
-                    } catch (SystemException e) {
-                        // Se non riesci a trovare il PokeLab, logga l'errore o gestisci la situazione
-                        System.err.println("Attenzione: PokeLab con plid " + plid + " non trovato.");
-                    }
-                }
+                // Recupera il PokeLab in un metodo separato
+                PokeLab pokeLab = fetchPokeLab(plid);
 
                 // Leggi i dati dell'utente
                 String username = resultSet.getString("username");
@@ -82,6 +75,23 @@ public class DBUserDAO implements UserDAO{
             throw new SystemException("Errore nella lettura dell'utente");
         }
     }
+
+    // Estrazione del blocco di codice che recupera il PokeLab
+    private PokeLab fetchPokeLab(int plid) throws SystemException {
+        // Verifica che plid non sia nullo (o zero)
+        if (plid > 0) {
+            try {
+                // Se plid è valido, prova a caricare il PokeLab
+                return new DBPokeLabDAO().read(plid);
+            } catch (SystemException e) {
+                // Se non riesci a trovare il PokeLab, logga l'errore o gestisci la situazione
+                System.err.println("Attenzione: PokeLab con plid " + plid + " non trovato.");
+                return null;  // Nessun PokeLab trovato, ma non lanciare eccezioni
+            }
+        }
+        return null;  // plid non valido, quindi non c'è un PokeLab associato
+    }
+
 
 
     @Override
@@ -118,15 +128,6 @@ public class DBUserDAO implements UserDAO{
         }
     }
 
-    private void deletePokeLab(User user) throws SystemException {
-        String query = "UPDATE users SET plid = NULL WHERE username = ?";
-        try (Connection connection = DBConnection.getDBConnection(); PreparedStatement preparedStatement = connection.prepareStatement(query)) {
-            preparedStatement.setString(1, user.getUsername());
-            preparedStatement.executeUpdate();
-        } catch (SQLException e) {
-            throw new SystemException("Errore delete poke lab!"+ e.getMessage());
-        }
-    }
 
     @Override
     public void delete (String uid) throws SystemException {
