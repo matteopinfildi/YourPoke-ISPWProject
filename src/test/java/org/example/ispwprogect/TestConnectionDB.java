cf1d51561338793b90574876b4e44f3379b0a211
matteopinfildi <pinfildimatteo@gmail.com>
2025-05-10 13:12:00 +0200

    Pulizia issues

--- src/test/java/org/example/ispwprogect/TestConnectionDB.java
+++ src/test/java/org/example/ispwprogect/TestConnectionDB.java
@@ -4,7 +4,7 @@ import org.example.ispwproject.utils.database.DBConnection;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-public class TestConnectionDB {
+class TestConnectionDB {
 
     @Test
     void testGetConnection() throws org.example.ispwproject.utils.exception.SystemException {
