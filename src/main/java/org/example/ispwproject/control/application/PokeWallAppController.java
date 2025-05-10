4d13c75815a942ffa04c81028e065242be72b2ce
matteopinfildi <pinfildimatteo@gmail.com>
2025-05-10 12:38:36 +0200

    Pulizia issues

--- src/main/java/org/example/ispwproject/control/application/PokeWallAppController.java
+++ src/main/java/org/example/ispwproject/control/application/PokeWallAppController.java
@@ -1,117 +1,3 @@
-//package org.example.ispwproject.control.application;
-//
-//import org.example.ispwproject.Session;
-//import org.example.ispwproject.SessionManager;
-//import org.example.ispwproject.control.graphic.pokewall.PokeWallController;
-//import org.example.ispwproject.model.PokeWallObserver;
-//import org.example.ispwproject.model.observer.pokewall.PokeWall;
-//import org.example.ispwproject.model.observer.pokewall.PokeWallDAO;
-//import org.example.ispwproject.utils.bean.PokeWallBean;
-//import org.example.ispwproject.utils.bean.SaveBean;
-//import org.example.ispwproject.model.user.User;
-//import org.example.ispwproject.model.user.UserDAO;
-//import org.example.ispwproject.utils.dao.DAOFactory;
-//import org.example.ispwproject.utils.exception.SystemException;
-//
-//import java.util.List;
-//
-//public class PokeWallAppController {
-//    private final PokeWallDAO pokeWallDAO;
-//    private final UserDAO userDAO;
-//    private static final PokeWallAppController instance = new PokeWallAppController();
-//
-//    private PokeWallAppController() {
-//        DAOFactory daoFactory = DAOFactory.getInstance();
-//        this.pokeWallDAO = daoFactory.getPokeWallDAO();
-//        this.userDAO = daoFactory.getUserDAO();
-//    }
-//
-//    public static PokeWallAppController getInstance() {
-//        return instance;
-//    }
-//
-//    public void registerObserver(PokeWallObserver observer, int sessionId) {
-//        try {
-//            String username = getUsernameFromSession(sessionId);
-//            if (observer instanceof PokeWallController pokeWallController) {
-//                pokeWallController.setCurrentUsername(username);
-//
-//                List<PokeWall> unseenPosts = pokeWallDAO.getUnseenPosts(username);
-//
-//                List<PokeWall> allPosts = pokeWallDAO.getAllPosts();
-//                for (PokeWall post : allPosts) {
-//                    post.removeObserver(observer);
-//                }
-//
-//                for (PokeWall post : unseenPosts) {
-//                    post.registerObserver(observer);
-//                    observer.update(post);
-//                    pokeWallDAO.markPostAsSeen(post.getId(), username);
-//                }
-//            }
-//        } catch (SystemException ignored) {
-//            // Puoi eventualmente loggare l'errore se hai un sistema di logging
-//        }
-//    }
-//
-//    public boolean createPost(SaveBean saveBean, PokeWallBean pokeWallBean) throws SystemException {
-//        String userId = saveBean.getUid();
-//        User user = userDAO.read(userId);
-//        if (user == null) {
-//            return false;
-//        }
-//
-//        PokeWall pokeWall = new PokeWall(0, pokeWallBean.getPokeName(), pokeWallBean.getSize(),
-//                user.getUsername(), pokeWallBean.getIngredients());
-//
-//        pokeWallDAO.create(pokeWall);
-//        pokeWall.notifyObservers();
-//
-//        return true;
-//    }
-//
-//    public List<PokeWall> getAllPosts() throws SystemException {
-//        return pokeWallDAO.getAllPosts();
-//    }
-//
-//    public boolean deletePost(int postId, String requestingUsername) throws SystemException {
-//        try {
-//            PokeWall postToDelete = getPostById(postId);
-//            if (postToDelete == null) {
-//                return false;
-//            }
-//
-//            if (!postToDelete.getUsername().equals(requestingUsername)) {
-//                return false;
-//            }
-//
-//            pokeWallDAO.delete(postId);
-//            return true;
-//        } catch (SystemException e) {
-//            throw e;
-//        }
-//    }
-//
-//    public PokeWall getPostById(int postId) throws SystemException {
-//        List<PokeWall> posts = pokeWallDAO.getAllPosts();
-//        for (PokeWall pokeWall : posts) {
-//            if (pokeWall.getId() == postId) {
-//                return pokeWall;
-//            }
-//        }
-//        return null;
-//    }
-//
-//    private String getUsernameFromSession(int sessionId) throws SystemException {
-//        Session session = SessionManager.getSessionManager().getSessionFromId(sessionId);
-//        User user = userDAO.read(session.getUserId());
-//        if (user == null) {
-//            throw new SystemException("User not found for session id: " + sessionId);
-//        }
-//        return user.getUsername();
-//    }
-//}
-
 package org.example.ispwproject.control.application;
 
 import org.example.ispwproject.Session;
@@ -128,6 +14,7 @@ import org.example.ispwproject.utils.exception.SystemException;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.logging.Logger;
 
 public class PokeWallAppController implements PokeWallObserver {
 
@@ -135,6 +22,7 @@ public class PokeWallAppController implements PokeWallObserver {
     private final UserDAO userDAO;
     private static final PokeWallAppController instance = new PokeWallAppController();
     private final List<PokeWallObserver> observers = new ArrayList<>();
+    private static final Logger logger = Logger.getLogger(PokeWallAppController.class.getName());
 
     private PokeWallAppController() {
         DAOFactory daoFactory = DAOFactory.getInstance();
@@ -162,8 +50,10 @@ public class PokeWallAppController implements PokeWallObserver {
                 observers.add(observer);  // Salviamo observer solo se non è già presente
             }
 
-        } catch (SystemException ignored) {
-            // TODO: logga eccezione
+        } catch (SystemException e) {
+            // Loggare l'eccezione con il logger
+            logger.severe("Error occurred while registering observer: " + e.getMessage());
+            e.printStackTrace();  // Opzionale, per visualizzare lo stack trace completo
         }
     }
 
