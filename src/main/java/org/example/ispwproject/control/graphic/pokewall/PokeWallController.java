18ec1222b1e3b0f5a2d375bff7d11a2949f5993f
matteopinfildi <pinfildimatteo@gmail.com>
2025-05-11 11:33:45 +0200

    Pulizia issues

--- src/main/java/org/example/ispwproject/control/graphic/pokewall/PokeWallController.java
+++ src/main/java/org/example/ispwproject/control/graphic/pokewall/PokeWallController.java
@@ -66,7 +66,7 @@ public class PokeWallController extends GraphicController {
                                     notificationLabel.setText("");
                                 });
                             } catch (InterruptedException e) {
-                                throw new RuntimeException("Thread was interrupted during sleep", e);
+                                logger.log(Level.SEVERE, "Thread was interrupted during sleep" + e.getMessage());
 
                             }
                         });
