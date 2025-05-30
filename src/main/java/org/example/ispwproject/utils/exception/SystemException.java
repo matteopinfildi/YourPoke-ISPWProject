89b6b32233805cdc4e6f53f68f8671cb167f4980
matteopinfildi <pinfildimatteo@gmail.com>
2025-05-30 15:44:45 +0200

    Modifica exception

--- src/main/java/org/example/ispwproject/utils/exception/SystemException.java
+++ src/main/java/org/example/ispwproject/utils/exception/SystemException.java
@@ -3,10 +3,10 @@ package org.example.ispwproject.utils.exception;
 // eccezione usata per indicare errori generici all'interno del sistema
 public class SystemException extends Exception {
   public SystemException() {
-    super("Oh no! Somehing wrong!");
+    super("Somehing wrong!");
   }
 
   public SystemException(String message) {
-    super("Technical error.\n" + message);
+    super("Error.\n" + message);
   }
 }
