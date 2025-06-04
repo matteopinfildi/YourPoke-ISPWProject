5e5bf814e845a505449a5e443a0471f69343bb57
matteopinfildi <pinfildimatteo@gmail.com>
2025-06-04 12:32:55 +0200

    Aggiunta commenti

--- src/main/java/org/example/ispwproject/control/application/LoginAppController.java
+++ src/main/java/org/example/ispwproject/control/application/LoginAppController.java
@@ -24,8 +24,10 @@ public class LoginAppController {
 
     public void register(UserBean userBean) throws LoginException {
 
+        // viene creato un oggetto di dominio, passando il costruttore di userBean
         User userA = new User(userBean);
         try {
+            // viene richiamato lo strato di persistenza per effettuare la create
             userDAO.create(userA);
         } catch (org.example.ispwproject.utils.exception.SystemException e) {
             throw new LoginException("Errore" + e.getMessage());
@@ -33,37 +35,51 @@ public class LoginAppController {
 
     }
 
+    // metodo per effettuare il login, restituisce -1 se il login fallisce
     public int login(CredentialBean credentialBean) throws LoginException {
         User userA = null;
 
+        // si recupera username e password
         String username = credentialBean.getUsername();
         String password = credentialBean.getPassword();
         try{
+            // si cerca l'utente sullo strato di persistenza
             userA = userDAO.read(username);
         } catch (org.example.ispwproject.utils.exception.SystemException e) {
             throw new LoginException("Errore" + e.getMessage());
         }
+
+        /* si controlla che lo user non sia null e che la password e lo username inseriti in input
+           siano uguali a quelli estratti dallo strato di persistenza */
         if (userA != null && userA.getUsername().equals(username) && userA.getPassword().equals(password)) {
+            // recupero dell'istanza singleton della sessione'
             SessionManager manager = SessionManager.getSessionManager();
-            // il controller passa dati all'esterno sotto forma di bean
+            // creazione di una nuova sessione
             Session session = manager.createSession(username);
+            // registrazione della sessione appena creata
             manager.addSession(session);
+            // viene restituito l'intero univoco associato alla sessione appena creata
             return session.getSessionId();
         }
+        // se qualcosa va storto ritorna -1
         return -1;
     }
 
     public boolean userType(UserTypeBean userTypeBean) throws LoginException {
         User userA = null;
 
+        // recupera l'id dell'utente
         String uid = userTypeBean.getUid();
+        // recupera il tipo di utente
         UserType uType = userTypeBean.getuType();
         try {
+            // recupero dell'utente associato all'id dallo strato di persistenza
             userA = userDAO.read(uid);
         } catch (org.example.ispwproject.utils.exception.SystemException e) {
             throw new LoginException("Errore" + e.getMessage());
         }
 
+        // confronto del valore atteso e il valore recuperato relativo allo userA
         return uType == userA.getuType();
     }
 }
